f6c3ad74035d5b00cca63461a51538d46dad44b6
Lisheng Zi <lisheng.zi@thoughtworks.com>
2020-11-15 23:08:12 +0800

    ok

--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -6,7 +6,10 @@ test("TODO 11", () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
 
-  // TODO 11: add assertion
+  //  11: add assertion
+  expect(mockCallback).toHaveBeenCalledTimes(2);
+  expect(mockCallback).toHaveReturnedWith(43);
+  expect(mockCallback).toHaveReturnedWith(44);
 });
 
 test("TODO 12", () => {
@@ -16,9 +19,9 @@ test("TODO 12", () => {
     .mockReturnValueOnce("string")
     .mockReturnValue(true);
 
-  // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  //  12: to add "expected" value
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe("string");
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -2,26 +2,46 @@ import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn();
+
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn().mockImplementation(() => {
+    return {
+      acceptInjection: mockAcceptInjection,
+      getHasAntibodies: mockGetHasAntibodies,
+    };
+  });
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockAcceptInjection.mockClear();
+  mockGetHasAntibodies.mockClear();
 });
-
+const vaccine = new VaccineTest();
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
-    // TODO 14: add test here
+    vaccine.inject();
+    //  14: add test here
+    expect(mockAcceptInjection).toHaveBeenCalledWith(
+      expect.any(Covid19Vaccine)
+    );
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
-    // TODO 15: add test here
+    //  15: add test here
+    mockGetHasAntibodies.mockReturnValueOnce(true);
+    expect(vaccine.test()).toBe("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
-    // TODO 16: add test here
+    // 16: add test here
+    mockGetHasAntibodies.mockReturnValueOnce(false);
+    expect(vaccine.test()).toBe("Vaccine Test Failed");
   });
 });
